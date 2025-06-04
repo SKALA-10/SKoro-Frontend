d1331fe6c5a47d51121c7be8bb91523f53f296a1
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-06-04 18:43:42 +0900

    Feat: 로그인 화면 배경에 존재하는 기하학적 도형 안 보이도록 변경

--- src/components/loginPage/BackgroundElements.tsx
+++ src/components/loginPage/BackgroundElements.tsx
@@ -12,14 +12,15 @@ const BackgroundElements: React.FC = () => (
     </div>
 
     {/* 배경 기하학적 도형들 */}
-    <div className="absolute inset-0" aria-hidden="true">
-      {/* <div className="absolute top-20 left-20 w-32 h-32 bg-blue-300 opacity-20 rounded-full blur-xl"></div> */}
+    {/* <div className="absolute inset-0" aria-hidden="true">
+      <div className="absolute top-20 left-20 w-32 h-32 bg-blue-300 opacity-20 rounded-full blur-xl"></div>
+
       <div className="absolute top-40 right-32 w-24 h-24 bg-white opacity-10 rounded-lg rotate-45"></div>
       <div className="absolute top-32 left-[10%] w-20 h-20 bg-blue-200 opacity-15 rounded-full"></div>
       <div className="absolute bottom-20 right-20 w-28 h-28 bg-white opacity-10 rounded-lg rotate-12"></div>
       <div className="absolute top-[48%] left-[24%] w-16 h-16 bg-blue-300 opacity-20 rounded-full"></div>
       <div className="absolute top-1/3 right-1/3 w-12 h-12 bg-white opacity-15 rounded-lg rotate-45"></div>
-    </div>
+    </div> */}
   </>
 )
 
