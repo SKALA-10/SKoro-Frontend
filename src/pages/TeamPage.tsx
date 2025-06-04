7f74a6e0c6f8c62657b9b23b4da1309d4f727cb7
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-06-04 18:54:41 +0900

    Feat: 팀 관리 화면에 대해 타이틀 설정

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,6 +1,7 @@
 import type { User } from '../types/TeamPage.types'
 import { TeamContent } from '../components/teamPage'
 import { Header } from '../components/common'
+import useDocumentTitle from '../hooks/useDocumentTitle'
 
 const currentUser: User = {
   name: '손지영',
@@ -9,6 +10,8 @@ const currentUser: User = {
 }
 
 const TeamPage: React.FC = () => {
+  useDocumentTitle('팀 관리 - SKoro')
+
   return (
     <div className="flex flex-1 flex-col min-h-0">
       <Header title="팀 관리 화면" user={currentUser} />
