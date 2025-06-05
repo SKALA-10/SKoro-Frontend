d057f8f5e9ccdbb4bcf2fe985294508a6b2228f3
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-06-05 14:07:05 +0900

    Feat: 리포트 유형 관련 타입 정의 파일

---
+++ src/types/Report.types.ts
@@ -0,0 +1,11 @@
+export interface Tab {
+  id: string
+  label: string
+  content: string
+}
+
+export interface FeedbackReportProps {
+  selectedYear: string
+  selectedRating: string
+  type: 'feedback' | 'final'
+}
