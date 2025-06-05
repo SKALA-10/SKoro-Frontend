7486541261384a5b5e55228004926f85688d4bf4
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-06-05 14:10:19 +0900

    Feat: 최종 평가 레포트 컨텐츠 컴포넌트

---
+++ src/components/finalPage/FinalContent.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { FilterSection, Report } from '../common'
+
+const FinalContent: React.FC = () => {
+  const [selectedYear, setSelectedYear] = useState('2025년도')
+  const [selectedRating, setSelectedRating] = useState('최종 평가')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  return (
+    <main className="flex-1 flex flex-col pb-5 px-10 overflow-hidden">
+      <FilterSection
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+        selectedRating={selectedRating}
+        setSelectedRating={setSelectedRating}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        filterType="final"
+      />
+      <Report
+        selectedYear={selectedYear}
+        selectedRating={selectedRating}
+        type="final"
+      />
+    </main>
+  )
+}
+
+export default FinalContent
