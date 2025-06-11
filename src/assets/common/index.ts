4347876469b959a161b19d9cfe2f326224e05394
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-06-11 09:42:38 +0900

    Chore: 나비 이미지 추가

--- src/assets/common/index.ts
+++ src/assets/common/index.ts
@@ -1,1 +1,3 @@
 export { default as ButterflyImg } from './img_butterfly.svg'
+export { default as ButterflyWhiteImg } from './img_butterfly_white.svg'
+export { default as ButterflyPurpleImg } from './img_butterfly_purple.svg'
